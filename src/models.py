5c289d62468693864962e5e567612450329fb60b
jarkor5 <67030533+jarkor5@users.noreply.github.com>
2023-08-18 00:45:35 +0000

    first commit

--- src/models.py
+++ src/models.py
@@ -7,26 +7,39 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class Usuario(Base):
+    __tablename__ = 'usuario'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    password = Column(String(250), nullable=False)
+    nombre = Column(String(250))
+    apellido = Column(String(250))
+    favoritos = relationship("Favorito")
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Planeta(Base):
+    __tablename__ = 'planeta'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    nombre = Column(String(250), nullable=False)
+    descripcion = Column(String(500))
+    favoritos = relationship("Favorito")
 
-    def to_dict(self):
-        return {}
+class Personaje(Base):
+    __tablename__ = 'personaje'
+    id = Column(Integer, primary_key=True)
+    nombre = Column(String(250), nullable=False)
+    especie = Column(String(250))
+    favoritos = relationship("Favorito")
 
-## Draw from SQLAlchemy base
+class Favorito(Base):
+    __tablename__ = 'favorito'
+    id = Column(Integer, primary_key=True)
+    usuario_id = Column(Integer, ForeignKey('usuario.id'))
+    planeta_id = Column(Integer, ForeignKey('planeta.id'))
+    personaje_id = Column(Integer, ForeignKey('personaje.id'))
+
+# Dibuja el diagrama de relación de entidades
 render_er(Base, 'diagram.png')
+
+# Crea la base de datos
+engine = create_engine('sqlite:///starwars_blog.db')
+Base.metadata.create_all(engine)
